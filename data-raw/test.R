4551a951347cd131799974053d8f1c6c42d49cf5
takewiki <hulilei@takewiki.com.cn>
2020-10-02 14:57:09 +0800

    V2.2

--- data-raw/test.R
+++ data-raw/test.R
@@ -5,6 +5,9 @@ library(DTedit)
 #建立链接
 conn <-conn_rds('nsic')
 
+fieldList <-sql_fieldInfo(conn,table='t_tsp_ques')
+View(fieldList)
+
 
 #测试链接
 # test_conn()
@@ -16,7 +19,7 @@ getBooks <- function(table='t_tsp_ques') {
   books <-sql_select(conn,sql)
   #print(books)
   #针对进行格式化处理
-  #如果出来新的数据类型，需要添加格式化函数 
+  #如果出来新的数据类型，需要添加格式化函数
   #请修改format_to_dtedit  --formatter.R
   fieldList <-sql_fieldInfo(conn,table)
   print(fieldList)
@@ -26,7 +29,7 @@ getBooks <- function(table='t_tsp_ques') {
     books[,i] <-format_to_dtedit(type)(books[,i])
     
   }
-
+  
   return(books)
 }
 
@@ -52,81 +55,47 @@ books.insert.callback <- function(data, row ,table='t_tsp_ques',f=getBooks,id_va
     }else{
       res[i] <- format_to_sqlInsert(type)(data[row,col_Insert])
     }
-
+    
   }
   sql_body <- paste0(res,collapse = ',')
   query <-paste0(sql_header,sql_body,")")
-
+  
   print(query) # For debugging
   sql_update(conn, query)
   return(f())
 }
 
-books.update.callback <- function(data, olddata, row,
+books.update.callback <- function(data,  row,
                                   table='t_tsp_ques',
-                                  f=getBooks,
+                                 # f=getBooks,
                                   edit.cols = c('FQues','FAnsw'),
                                   id_var='FId') 
-  {
+{
   sql_header <- sql_gen_update(table);
   print(sql_header)
   fieldList <-sql_fieldInfo(conn,table)
   ncount <-length(edit.cols)
   res <- character(ncount)
   for (i in 1:ncount){
+    print(i)
     col_Update <-edit.cols[i]
+    print(col_Update)
     #col_Insert <-fieldList[fieldList$,'FFieldName']
     type <-fieldList[fieldList$FFieldName == col_Update,'FTypeName']
+    print(type)
+    
     res[i] <- paste0(' ',col_Update,' = ',format_to_sqlUpdate(type)(data[row,col_Update]))
-   
+    print(res[i])
+    
     
   }
   sql_body <- paste0(res,collapse = ',')
   print(sql_body)
   sql_tail <-paste0(' where ',id_var,' = ',data[row,id_var])
   query <- paste0(sql_header,sql_body,sql_tail)
-
+  
   print(query) # For debugging
-  sql_update(conn, query)
-  return(f())
+  #sql_update(conn, query)
+  #return(f())
 }
 
-books.delete.callback <- function(data, row ,table ='t_tsp_ques',f=getBooks,id_var='FId') {
-  sql_header <- sql_gen_delete(table);
-  sql_tail <-paste0('  ',id_var,' = ',data[row,id_var])
-  query <- paste0(sql_header,sql_tail)
-  
-  #query <- paste0("DELETE FROM  ",table,"  WHERE id = ", data[row,]$id)
-  print(query)
-  sql_update(conn, query)
-  return(f())
-}
-
-##### Create the Shiny server
-server <- function(input, output) {
-       books <- getBooks()
-       print(books)
-  dtedit2(input, output,
-         name = 'books',
-         thedata = books,
-         edit.cols = c('FQues','FAnsw'),
-         edit.label.cols = c('问题','答案'),
-         input.types = c(FAnsw='textAreaInput'),
-         #input.choices = list(FNUMBER = unique(unlist(books$FNUMBER))),
-         view.cols = c('FId','FQues','FAnsw'),
-         view.captions = c('序号','问题','答案'),
-         callback.update = books.update.callback,
-         callback.insert = books.insert.callback,
-         callback.delete = books.delete.callback)
-  
-
-}
-
-##### Create the shiny UI
-ui <- fluidPage(
-  h3('Books'),
-  uiOutput('books'),
-  hr()
-)
-
-shinyApp(ui = ui, server = server)
